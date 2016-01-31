34738943232eb60a26a425e781489e1c77040546
Sascha <leuthold@tr-lap352.prog.altair.com>
2016-01-31 13:24:51 -0800

    Functional Variable add/remove form
    No access to levels and bounds, they are deeper in the HstItem hierachy

--- public/javascripts/variables.js
+++ public/javascripts/variables.js
@@ -9,17 +9,62 @@ function loadTable() {
         url: "/hyperstudy/api/studies/" + study + "/variables",
         cache: false,
         success: function (data) {
-            /*var table = $("#list").find('tbody');
-            data.forEach(function (element) {
-                table.append($('<tr>').attr('data-id', element.study)
-                    .append($('<td>').text(element.study))
-                    .append($('<td>').text(element.directory))
-                    );
-            });*/
+            data.variables.forEach(function (element) {
+                addRow(element);
+            });
+        }
+    });
+}
+
+function addRow(obj) {
+    var table = $("#list").find('tbody');
+    var tr = $('<tr>').attr('data-id', obj.Varname);
+    tr.append($('<td>').text(obj.State));
+    tr.append($('<td>').text(obj.Label));
+    tr.append($('<td>').text(obj.Varname));
+    tr.append($('<td>').text(obj.Equation));
+    tr.append($('<td>').text(obj.ParamRefAttr));
+    tr.append($('<td>').text(obj.Category));
+    tr.append($('<td>').text(obj.Role));
+    tr.append($('<td>').text(obj.Comment));
+    tr.append($('<input id="remove" type="button" size="20" value="Remove">').on('click', function () {
+        removeVariable(obj.Varname, function () {
+            $('*[data-id="' + obj.Varname + '"]').remove();
+        });
+    }))
+    table.append(tr);
+}
+
+function newVariable() {
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        processData: false,
+        url: "/hyperstudy/api/studies/" + study + "/variables",
+        cache: false,
+        success: function (data) {
+            addRow(data);
+        }
+    });
+}
+
+function removeVariable(id, callback) {
+    $.ajax({
+        type: "DELETE",
+        dataType: "json",
+        url: "/hyperstudy/api/studies/" + study + "/variables/" + id,
+        cache: false,
+        success: function (data) {
+            callback();
         }
     });
 }
 
 $(document).ready(function () {
+    $(document).ready(function () {
+        $('#new').bind('click', function () {
+            newVariable();
+        });
+    });
     loadTable();
 });
