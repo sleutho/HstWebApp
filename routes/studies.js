adb64b768d02b3534bdf28e4f20ff2f33fc97470
Sascha <saleuth@gmx.de>
2016-02-03 19:54:34 -0800

    CSS & navigation

--- routes/studies.js
+++ routes/studies.js
@@ -9,49 +9,49 @@ router.get('/', function (req, res, next) {
 
 router.get('/:study/studyindex', function (req, res, next) {
     res.render('studyindex', {
-        title: 'Study ( ' + req.params.study + ' )',
+        title: 'Index',
         study: req.params.study
     });
 });
 
 router.get('/:study', function (req, res, next) {
     res.render('study', {
-        title: 'Study ( ' + req.params.study + ' )',
+        title: 'Study',
         study: req.params.study
     });
 });
 
 router.get('/:study/variables', function (req, res, next) {
     res.render('variables', {
-        title: 'Variables of Study ( ' + req.params.study + ' )',
+        title: 'Variables',
         study: req.params.study
     });
 });
 
 router.get('/:study/responses', function (req, res, next) {
     res.render('responses', {
-        title: 'Responses of Study ( ' + req.params.study + ' )',
+        title: 'Responses',
         study: req.params.study
     });
 });
 
 router.get('/:study/specification', function (req, res, next) {
     res.render('specification', {
-        title: 'Specification ( ' + req.params.study + ' )',
+        title: 'Specification',
         study: req.params.study
     });
 });
 
 router.get('/:study/evaluate', function (req, res, next) {
     res.render('evaluate', {
-        title: 'Evaluate Study ( ' + req.params.study + ' )',
+        title: 'Evaluate',
         study: req.params.study
     });
 });
 
 router.get('/:study/summary', function (req, res, next) {
     res.render('summary', {
-        title: 'Summary of Study ( ' + req.params.study + ' )',
+        title: 'Summary',
         study: req.params.study
     });
 });
