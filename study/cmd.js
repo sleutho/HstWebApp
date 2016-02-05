a9f2688bf298daf2d9d96dc57e3ba047ac03bd3f
Sascha <saleuth@gmx.de>
2016-02-05 06:13:46 -0800

    Hardcoded path to hstbatch

--- study/cmd.js
+++ study/cmd.js
@@ -3,14 +3,20 @@ var path = require('path');
 var studylist = require('./studylist');
 
 exports.getHstPath = function () {
-    return path.join(studylist.getDataHome(), 'test.bat');
+    //return path.join(studylist.getDataHome(), 'test.bat');
+    return "j:/Software/HstFbEuclid-04_tr_lap352/hst/bin/win64d/hstbatch.exe"
 }
 
 exports.execHelperDir = function (studyDir, args, callback) {
-    execFile(exports.getHstPath(), args, {cwd:studyDir}, (error, stdout, stderr) => {
+    execFile(exports.getHstPath(), args, { cwd: studyDir }, (error, stdout, stderr) => {
         if (error) {
+            console.log("EXEC Err" + error);
             return callback(error, null);
         }
+        if (stderr.length) {
+            console.log("EXEC Err" + stderr.toString());
+        }
+        console.log("EXEC Dir " + stdout.length ? stdout.toString() : null);
         callback(
             stderr.length ? stderr.toString() : null,
             stdout.length ? stdout.toString() : null);
