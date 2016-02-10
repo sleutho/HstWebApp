b4c98b6809096e2b72b174cd7640ebe3118a395d
Sascha <saleuth@gmx.de>
2016-02-09 21:01:19 -0800

    Remove a comment

--- public/javascripts/responses.js
+++ public/javascripts/responses.js
@@ -88,7 +88,6 @@ function editCell () {
       $.ajax({
         type: 'PUT',
         url: '/hyperstudy/api/studies/' + study + '/responses/' + varname,
-        // data: 'attr=' + attr + '&value=' + newContent,
         data: { attr: attr, value: newContent },
         cache: false,
         success: function (data) {
